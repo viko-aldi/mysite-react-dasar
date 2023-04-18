8df3b98a83f878624662f519dc8cdaf6ce64e114
UsernameAnda <piccoaldi@gmail.com>
2023-04-18 20:19:12 +0700

    Coomit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import Welcome from './Welcome';
 
-function App() {
+// function App() {
+
+const App = () =>{
+  const [name, setName] = useState("John");
+  const [gender, setGender] = useState('laki-laki');
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      {gender === "laki-laki" ? <span>Pak</span> : <span>Buk</span>}
+      <span>{name}</span>
     </div>
-  );
+   
+  )
 }
 
 export default App;
